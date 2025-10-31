dd91fa00ad58f346db896289cba6199a83140402
Rutu Makadiya <rutumakadiya2000@gmail.com>
2025-10-31 09:22:04 +0000

    scroll down karu try header pachad content dekahay che pn mare evo ny jo

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -46,7 +46,7 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-sm shadow-sm">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-background shadow-sm">
       <div className="max-w-[1200px] mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center h-20">
           <Logo />
