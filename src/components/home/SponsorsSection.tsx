f1fab1a9c52aa0d76df3d954222da0daed85cf76
Rutu Makadiya <rutumakadiya2000@gmail.com>
2025-10-31 12:00:29 +0000

    set these image properly it not looking good

--- src/components/home/SponsorsSection.tsx
+++ src/components/home/SponsorsSection.tsx
@@ -13,10 +13,10 @@ const SponsorsSection = () => {
           We thank our supporters for being part of our journey.
         </p>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 items-center justify-items-center">
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-8 items-center justify-items-center">
         {sponsorImages.map(sponsor => (
-          <a key={sponsor.id} href="#" className="flex justify-center items-center p-2 rounded-lg opacity-70 hover:opacity-100 transition-opacity w-40 h-20">
-            <div className="relative w-full h-full">
+          <a key={sponsor.id} href="#" className="flex justify-center items-center p-4 rounded-lg opacity-70 hover:opacity-100 transition-opacity">
+            <div className="relative w-32 h-16">
               <Image
                 src={sponsor.imageUrl}
                 alt={sponsor.description}
