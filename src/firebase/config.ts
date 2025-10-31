cc8f6d256d1b39abac0d416e9f9a6bf22c2545d3
Rutu Makadiya <rutumakadiya2000@gmail.com>
2025-10-31 09:44:26 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBIb8VKTSMyRz7fA0l9FKkna33LOkuN

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8466476924-ce943",
-  "appId": "1:886653697105:web:91961f7400f61b8c6b0c70",
-  "apiKey": "AIzaSyBIb8VKTSMyRz7fA0l9FKkna33LOkuNfcY",
-  "authDomain": "studio-8466476924-ce943.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "886653697105"
+  apiKey: "AIzaSyBIb8VKTSMyRz7fA0l9FKkna33LOkuNfcY",
+  authDomain: "studio-8466476924-ce943.firebaseapp.com",
+  projectId: "studio-8466476924-ce943",
+  storageBucket: "studio-8466476924-ce943.appspot.com",
+  messagingSenderId: "886653697105",
+  appId: "1:886653697105:web:91961f7400f61b8c6b0c70"
 };
