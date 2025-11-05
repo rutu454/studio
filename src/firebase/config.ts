3d9511ed100126d6cead4418fcecda0c2187e422
Rutu Makadiya <rutumakadiya2000@gmail.com>
2025-11-05 13:02:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8466476924-ce943",
+  "appId": "1:886653697105:web:fbcf9c24dff8ebb86b0c70",
+  "apiKey": "AIzaSyBIb8VKTSMyRz7fA0l9FKkna33LOkuNfcY",
+  "authDomain": "studio-8466476924-ce943.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "886653697105"
+};
