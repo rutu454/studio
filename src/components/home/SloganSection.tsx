0331e3d182e371ade79d957a7ea8d9383acf15a8
Rutu Makadiya <rutumakadiya2000@gmail.com>
2025-11-06 07:20:08 +0000

    shologon section background color #CC0000 and text #f0f0f0f0

--- src/components/home/SloganSection.tsx
+++ src/components/home/SloganSection.tsx
@@ -2,10 +2,10 @@ import SectionWrapper from '../common/SectionWrapper';
 
 const SloganSection = () => {
   return (
-    <div className="bg-muted">
+    <div className="bg-[#CC0000]">
       <SectionWrapper>
         <div className="text-center">
-          <h2 className="text-3xl md:text-5xl font-bold font-headline tracking-tight">
+          <h2 className="text-3xl md:text-5xl font-bold font-headline tracking-tight text-[#f0f0f0]">
             “Together we rise, together we grow.”
           </h2>
         </div>
